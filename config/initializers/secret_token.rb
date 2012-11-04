81a9d9797b5e3d21430aeccc85a06a1f83f4f154
Herve <h.leroy@me.com>
2012-11-04 17:59:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideotron::Application.config.secret_token = 'ef0bf9fd47d63e0d62ba7fb4e657f18188e1f16459c2e865d6555af5d1bc25752e4f85976e93884baa4989c8a2a843d93b0fd81512a325245cd724da468efd58'
